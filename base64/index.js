389ea3cd671059861876ec78b8deb946d733832b
ReyZ19 <77238876+ReyZ19@users.noreply.github.com>
2021-03-17 18:34:13 +0300

    Update index.js

--- base64/index.js
+++ base64/index.js
@@ -1,8 +1,11 @@
+// Encode
 function enCode() {
     var x = document.getElementById("enText").value;
     var result = btoa(x)
     alert(`Encode Result: ` + result)
 }
+
+//Decode
 function deCode() {
     var x = document.getElementById("deText").value;
     var result = atob(x)
