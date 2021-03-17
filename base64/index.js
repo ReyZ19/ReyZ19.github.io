33450185a276a3bfe5255bb85757e8a84f97aacc
ReyZ19 <77238876+ReyZ19@users.noreply.github.com>
2021-03-17 22:43:51 +0300

    Update index.js

--- base64/index.js
+++ base64/index.js
@@ -3,8 +3,8 @@ function enCode() {
     var x = document.getElementById("enText").value;
     console.log("Encode Result: " + btoa(x))
     document.getElementById("deText").value = btoa(x);
-	document.getElementById("deText").style = "background-color: cornflowerblue;"
-	setTimeout(() => { document.getElementById("deText").style = "background-color: #cccccc;" }, 2000);
+    document.getElementById("deText").style = "background-color: cornflowerblue;"
+    setTimeout(() => { document.getElementById("deText").style = "background-color: #cccccc;" }, 2000);
 }
 
 // Decode
@@ -12,6 +12,6 @@ function deCode() {
     var x = document.getElementById("deText").value;
     console.log("Decode Result: " + atob(x))
     document.getElementById("enText").value = atob(x);
-	document.getElementById("enText").style = "background-color: cornflowerblue;" 
-	setTimeout(() => { document.getElementById("enText").style = "background-color: #cccccc;" }, 2000);
+    document.getElementById("enText").style = "background-color: cornflowerblue;" 
+    setTimeout(() => { document.getElementById("enText").style = "background-color: #cccccc;" }, 2000);
 }
