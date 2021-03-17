e82a356ee6f437726a7dc6cfb0c873b7ac320899
ReyZ19 <77238876+ReyZ19@users.noreply.github.com>
2021-03-17 22:43:22 +0300

    Update index.js

--- base64/index.js
+++ base64/index.js
@@ -1,13 +1,17 @@
 // Encode
 function enCode() {
     var x = document.getElementById("enText").value;
-    alert(`Encode Result: ` + btoa(x))
     console.log("Encode Result: " + btoa(x))
+    document.getElementById("deText").value = btoa(x);
+	document.getElementById("deText").style = "background-color: cornflowerblue;"
+	setTimeout(() => { document.getElementById("deText").style = "background-color: #cccccc;" }, 2000);
 }
 
 // Decode
 function deCode() {
     var x = document.getElementById("deText").value;
-    alert(`Decode Result: ` + atob(x))
     console.log("Decode Result: " + atob(x))
+    document.getElementById("enText").value = atob(x);
+	document.getElementById("enText").style = "background-color: cornflowerblue;" 
+	setTimeout(() => { document.getElementById("enText").style = "background-color: #cccccc;" }, 2000);
 }
