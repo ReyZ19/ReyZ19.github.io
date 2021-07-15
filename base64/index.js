3fc21e1d7abf44fc04a9e7ec56391e4ae6c57f13
ReyZ19 <77238876+ReyZ19@users.noreply.github.com>
2021-07-15 17:25:17 +0300

    Update index.js

--- base64/index.js
+++ base64/index.js
@@ -14,17 +14,26 @@
 // Encode
 function enCode() {
     var x = document.getElementById("enText").value;
-    console.log("Encode Result: " + btoa(x))
-    document.getElementById("deText").value = btoa(x);
-    document.getElementById("deText").style = "background-color: cornflowerblue;"
+    try {
+     console.log("Encode Result: " + btoa(x));
+     document.getElementById("deText").value = btoa(x);
+    }
+    catch(err) {
+     document.getElementById("deText").value = "Error!";
+    }
+    document.getElementById("deText").style = "background-color: cornflowerblue;";
     setTimeout(() => { document.getElementById("deText").style = "background-color: #cccccc;" }, 2000);
 }
-
 // Decode
 function deCode() {
     var x = document.getElementById("deText").value;
-    console.log("Decode Result: " + atob(x))
-    document.getElementById("enText").value = atob(x);
-    document.getElementById("enText").style = "background-color: cornflowerblue;" 
+    try {
+     console.log("Decode Result: " + atob(x));
+     document.getElementById("enText").value = atob(x);
+    }
+    catch(err) {
+     document.getElementById("enText").value = "Error!";
+    }
+    document.getElementById("enText").style = "background-color: cornflowerblue;";
     setTimeout(() => { document.getElementById("enText").style = "background-color: #cccccc;" }, 2000);
 }
